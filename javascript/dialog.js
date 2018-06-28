2f2a4cdda5f79a0e03837761e16f5a077669cee5
Wilbert <wilbert@spearpoint.nl>
2018-06-28 11:01:01 +0200

    Bug fix dialog

--- javascript/dialog.js
+++ javascript/dialog.js
@@ -22,11 +22,15 @@
     _this.dialog = $("<div />", { "class": "dialog dialog--alert" });
 
     if (_this.settings.title != '') _this.dialog.append($("<div />", { "class": "dialog__header" }).html(_this.settings.title));
-    _this.dialog.append($("<div />", { "class": "dialog__content" }).html(_this.settings.content.replace(/\n/g, "<br />")));
+    if (typeof _this.settings.content.replace === "function") {
+        _this.settings.content = _this.settings.content.replace(/\n/g, "<br />");
+    }
+    _this.dialog.append($("<div />", { "class": "dialog__content" }).html(_this.settings.content));
+
 
     _this.buttons = $("<div />", { "class": "dialog__buttons" });
     for (i = 0; i < _this.settings.buttons.length; i++) {
-        var button = $("<button />", { "class": "flat-button "+ _this.settings.buttons[i].class }).text(_this.settings.buttons[i].text);
+        var button = $("<button />", { "class": "flat-button " + _this.settings.buttons[i].class }).text(_this.settings.buttons[i].text);
         if (_this.settings.buttons[i].onClick) button.on("click", _this.settings.buttons[i].onClick);
         button.on("click", function () {
             _this.close();
